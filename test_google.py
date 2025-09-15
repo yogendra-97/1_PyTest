d2a6bdffebc27452022a606eb02d2c2a08cae4b2
yogendra-97 <yogendranarayanp7799@gmail.com>
2025-09-15 17:28:21 +0530

    'Flipkart' added

--- test_google.py
+++ test_google.py
@@ -13,6 +13,6 @@ def test_Apple(browser):
 
     driver=browser
     driver.get("https://www.google.com/")
-    driver.find_element(By.ID, "APjFqb").send_keys('Apple 3')
+    driver.find_element(By.ID, "APjFqb").send_keys('flipkart')
     time.sleep(2)
     driver.quit()

a09a9d863a536bf64ef32bf50faa8af830bfc9ae
yogendra-97 <yogendranarayanp7799@gmail.com>
2025-09-15 17:29:41 +0530

    'google' added

--- test_google.py
+++ test_google.py
@@ -13,6 +13,6 @@ def test_Apple(browser):
 
     driver=browser
     driver.get("https://www.google.com/")
-    driver.find_element(By.ID, "APjFqb").send_keys('Apple 2')
+    driver.find_element(By.ID, "APjFqb").send_keys('google')
     time.sleep(2)
     driver.quit()

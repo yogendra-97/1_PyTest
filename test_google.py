7bf77f3fbad67a2df7adbcdb48e7feee414715d8
yogendra-97 <yogendranarayanp7799@gmail.com>
2025-09-16 16:55:09 +0530

    Live Changes on Remote

--- test_google.py
+++ test_google.py
@@ -13,6 +13,6 @@ def test_Apple(browser):
 
     driver=browser
     driver.get("https://www.google.com/")
-    driver.find_element(By.ID, "APjFqb").send_keys('Automation')
+    driver.find_element(By.ID, "APjFqb").send_keys('This is origin/main')
     time.sleep(2)
     driver.quit()

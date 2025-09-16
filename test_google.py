0b096607adba7722e7cac8f07532a66ede60bc13
yogendra-97 <yogendranarayanp7799@gmail.com>
2025-09-16 17:39:10 +0530

    ok

--- test_google.py
+++ test_google.py
@@ -13,6 +13,6 @@ def test_Apple(browser):
 
     driver=browser
     driver.get("https://www.google.com/")
-    driver.find_element(By.ID, "APjFqb").send_keys('Github')
+    driver.find_element(By.ID, "APjFqb").send_keys('Github Desktop')
     time.sleep(2)
     driver.quit()

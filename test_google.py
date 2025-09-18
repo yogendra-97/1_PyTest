5235f1b4235b5455a7f41328bed53527e7c187d6
yogendra-97 <yogendranarayanp7799@gmail.com>
2025-09-18 15:32:56 +0530

    Random Add

--- test_google.py
+++ test_google.py
@@ -13,6 +13,6 @@ def test_Apple(browser):
 
     driver=browser
     driver.get("https://www.google.com/")
-    driver.find_element(By.ID, "APjFqb").send_keys('Automation-Google Branch')
+    driver.find_element(By.ID, "APjFqb").send_keys('Random')
     time.sleep(2)
     driver.quit()

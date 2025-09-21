7ce6e7fdac9a484c35d5bcfcd939c45323f45d2e
yogendra-97 <yogendranarayanp7799@gmail.com>
2025-09-21 18:39:30 +0530

    Update test_google.py

--- test_google.py
+++ test_google.py
@@ -13,6 +13,6 @@ def test_Apple(browser):
 
     driver=browser
     driver.get("https://www.google.com/")
-    driver.find_element(By.ID, "APjFqb").send_keys('Random')
+    driver.find_element(By.ID, "APjFqb").send_keys('Google_Test')
     time.sleep(2)
     driver.quit()

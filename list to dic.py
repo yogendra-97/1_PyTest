a824d68847bb4c79d089758c5449ed8b743d750c
yogendra-97 <yogendranarayanp7799@gmail.com>
2025-09-21 18:38:14 +0530

    Update list to dic.py

--- list to dic.py
+++ list to dic.py
@@ -1,21 +1,3 @@
-# from operator import length_hint
-#
-# import openpyxl
-#
-# book = openpyxl.load_workbook("C:\\Users\\helpe\\Desktop\\test.xlsx")
-# sheet = book.active
-# cell = sheet.cell(row=1, column=1).value
-# cell2= sheet.cell(row=2, column=1).value
-#
-# l1= ["username", "password"]
-# l2= [cell, cell2]
-#
-# dt={}
-#
-# for i in range(2):
-#     dt[i]=l2[i-1]
-#
-# print(dt)
 
 arr = [1, 0, 0, 1, 1, 0, 1, 0]
 
